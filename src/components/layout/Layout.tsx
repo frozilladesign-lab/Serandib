561c752582b714c31f5c20fe11049b551870b55f
SachinRasangika <70104318+SachinRasangika@users.noreply.github.com>
2026-07-16 06:51:07 +0530

    Remove countdown overlay feature

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,14 +3,12 @@ import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { WhatsAppButton } from './WhatsAppButton';
-import { CountdownOverlay } from './CountdownOverlay';
 import { WelcomeModal } from '../personalization/WelcomeModal';
 import { useScreenInit } from '../../useScreenInit';
 export function Layout() {
   useScreenInit();
   return (
     <div className="min-h-screen flex flex-col font-body bg-light">
-      <CountdownOverlay />
       <WelcomeModal />
       <Header />
       <main className="flex-grow">
